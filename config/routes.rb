43597e07d61479c4c13b36028f882c747185bf08
Sunny <ring2rings93@gmail.com>
2019-04-25 23:42:24 +0100

    added named routes, relative path and refactored templates with route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'staticpages/home'
-  get 'staticpages/help'
-  get 'staticpages/about'
   root 'staticpages#home'
+
+  get '/help', to: 'staticpages#help'
+  get '/about', to: 'staticpages#about'
+  get '/contact', to: 'staticpages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
