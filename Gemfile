f5a89a75644d1df0326b056ff8b74c38a92d6677
Sunny <ring2rings93@gmail.com>
2019-05-07 00:07:55 +0100

    added index,edit,update,delete methods

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ ruby '2.6.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 gem 'bcrypt','3.1.12'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
+
 # Use sqlite3 as the database for Active Record
 
 # Use Puma as the app server
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,4 +11,42 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #   assert_response :success
   # end
 
+
+  def setup
+    @user = users(:sunny)
+    @other_user = users(:michael)
+  end
+  
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  
+# test "should not allow the admin attribute to be edited via the web" do
+#     log_in_as(@other_user)
+#     assert_not @other_user.admin?
+#     patch user_path(@other_user), params: {
+#                                     user: { password:              FILL_IN,
+#                                             password_confirmation: FILL_IN,
+#                                             admin: FILL_IN } }
+#     assert_not @other_user.FILL_IN.admin?
+#   end
+
+
+
+
 end
